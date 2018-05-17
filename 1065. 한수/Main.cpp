dccf137c403db6f929f1d084ec5e3269e7934bf0
Stareat0707 <stareat0707@gmail.com>
2018-05-17 10:16:53 +0900

    번호 카운트 룰 변경

---
+++ 1065. 한수/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	int cnt = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		while (true)
+		{
+
+		}
+	}
+
+	std::cout << cnt << std::endl;
+	return 0;
+}
