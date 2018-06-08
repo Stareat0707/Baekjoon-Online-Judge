4aee82c9476871e06b1d4b3bfcbba46dfbe697e3
Stareat0707 <stareat0707@gmail.com>
2018-06-08 10:55:50 +0900

    2747번 문제 해결

---
+++ 02747. 피보나치 수/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	int* fibonacci = new int[n + 1];
+	fibonacci[0] = 0;
+	fibonacci[1] = 1;
+	for (int i = 2; i <= n; ++i)
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+	std::cout << fibonacci[n] << std::endl;
+	return 0;
+}
